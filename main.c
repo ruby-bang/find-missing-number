68f15c4588f2f5dcb1179554642f0752cfa577d9
ruby-bang <rubyegt@gmail.com>
2024-02-04 18:09:17 +0700

    fix : make some code little short

--- main.c
+++ main.c
@@ -5,11 +5,7 @@ int compare_ints(const void *a, const void *b) {
   int arg1 = *(const int *)a;
   int arg2 = *(const int *)b;
 
-  if (arg1 < arg2)
-    return -1;
-  if (arg1 > arg2)
-    return 1;
-  return 0;
+  return arg1 < arg2 ? -1 : 1;
 }
 
 int main() {
